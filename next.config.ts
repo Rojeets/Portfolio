2c1d655993eeb2dd8e5866d247c9a8b8c720e393
Rojit Pokharel <pokharelrojit45@gmail.com>
2026-05-13 21:47:13 +0545

    refactor: remove turbopack configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  turbopack: {
-    root: '/home/jimmy/Documents/portfolio'
-  }
 }
 
 export default nextConfig
